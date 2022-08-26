827ab80e31d734b32923efba41bb811193137a4c
xuexin <xuexin@guanghe.tv>
2022-08-26 15:30:37 +0800

    mq-2.0

--- amqp/index.go
+++ amqp/index.go
@@ -12,22 +12,27 @@ var mqChan *amqp.Channel
 
 // RabbitMQ 定义RabbitMQ对象
 type RabbitMQ struct {
-	connectConf ConnectConf
-	queueConf   QueueExchange // 队列配置
-	connection  *amqp.Connection
-	channel     *amqp.Channel
-	mu          sync.RWMutex
-	rabbitUrl   string
+	ConnectConf         ConnectConf
+	ProducerRoutingConf ProducerRoutingConf
+	ConsumerQueueConf   ConsumerQueueConf
+	Connection          *amqp.Connection
+	Channel             *amqp.Channel
+	Mu                  sync.RWMutex
+	RabbitUrl           string
 }
 
 // New 创建一个新的操作对象
-func New(connect ConnectConf, queueConf QueueExchange) *RabbitMQ {
-	if queueConf.ExchangeType == "" {
-		queueConf.ExchangeType = ExchangeTopic
+func New(connect ConnectConf, producerRoutingConf ProducerRoutingConf, consumerQueueConf ConsumerQueueConf) *RabbitMQ {
+	if len(producerRoutingConf.ExchangeType) == 0 {
+		producerRoutingConf.ExchangeType = ExchangeTopic
+	}
+	if len(consumerQueueConf.ExchangeType) == 0 {
+		consumerQueueConf.ExchangeType = ExchangeTopic
 	}
 	return &RabbitMQ{
-		connectConf: connect,
-		queueConf:   queueConf,
+		ConnectConf:         connect,
+		ProducerRoutingConf: producerRoutingConf,
+		ConsumerQueueConf:   consumerQueueConf,
 	}
 }
 
@@ -35,14 +40,14 @@ func New(connect ConnectConf, queueConf QueueExchange) *RabbitMQ {
 func (r *RabbitMQ) MqConnect() (*RabbitMQ, error) {
 	var err error
 	r.GetRabbitUrl()
-	mqConn, err = amqp.Dial(r.rabbitUrl)
-	r.connection = mqConn // 赋值给RabbitMQ对象
+	mqConn, err = amqp.Dial(r.RabbitUrl)
+	r.Connection = mqConn // 赋值给RabbitMQ对象
 	if err != nil {
-		fmt.Printf("RabbitMQ链接失败url:%s ---- err:%s \n", r.rabbitUrl, err)
+		fmt.Printf("RabbitMQ链接失败url:%s ---- err:%s \n", r.RabbitUrl, err)
 		return r, err
 	}
 	mqChan, err = mqConn.Channel()
-	r.channel = mqChan // 赋值给RabbitMQ对象
+	r.Channel = mqChan // 赋值给RabbitMQ对象
 	if err != nil {
 		fmt.Printf("RabbitMQ管道失败:%s \n", err)
 	}
@@ -53,14 +58,14 @@ func (r *RabbitMQ) MqConnect() (*RabbitMQ, error) {
 func (r *RabbitMQ) MqClose() error {
 	var err error
 	// 先关闭管道,再关闭链接
-	if r.channel != nil {
-		err = r.channel.Close()
+	if r.Channel != nil {
+		err = r.Channel.Close()
 		if err != nil {
 			fmt.Printf("RabbitMQ管道关闭失败:%s \n", err)
 		}
 	}
-	if r.connection != nil {
-		err = r.connection.Close()
+	if r.Connection != nil {
+		err = r.Connection.Close()
 		if err != nil {
 			fmt.Printf("RabbitMQ链接关闭失败:%s \n", err)
 		}
@@ -78,7 +83,7 @@ func (r *RabbitMQ) Producer(msg string) error {
 	}
 
 	// 验证链接是否正常,否则重新链接
-	if r.channel == nil {
+	if r.Channel == nil {
 		_, err = r.MqConnect()
 		if err != nil {
 			return err
@@ -93,14 +98,14 @@ func (r *RabbitMQ) Producer(msg string) error {
 	// internal:是否为内部
 	// noWait:是否非阻塞, true为是,不等待RMQ返回信息;
 	// args:参数,传nil即可;
-	err = r.channel.ExchangeDeclare(r.queueConf.ExchangeName, r.queueConf.ExchangeType, true, false, false, false, nil)
+	err = r.Channel.ExchangeDeclare(r.ProducerRoutingConf.ExchangeName, string(r.ProducerRoutingConf.ExchangeType), true, false, false, false, nil)
 	if err != nil {
 		fmt.Printf("RabbitMQ注册交换机失败:%s \n", err)
 		return err
 	}
 
 	// 发送任务消息
-	err = r.channel.Publish(r.queueConf.ExchangeName, r.queueConf.RoutingKey, false, false, amqp.Publishing{
+	err = r.Channel.Publish(r.ProducerRoutingConf.ExchangeName, r.ProducerRoutingConf.RoutingKey, false, false, amqp.Publishing{
 		ContentType: "text/plain",
 		Body:        []byte(msg),
 	})
@@ -115,7 +120,7 @@ func (r *RabbitMQ) Producer(msg string) error {
 // Consumer 接收任务消费消息 接收指定队列指定路由的数据接收者
 func (r *RabbitMQ) Consumer(doFunc func(string) error) {
 	// 验证链接是否正常,否则重新链接
-	if r.channel == nil {
+	if r.Channel == nil {
 		_, err := r.MqConnect()
 		if err != nil {
 			return
@@ -130,14 +135,14 @@ func (r *RabbitMQ) Consumer(doFunc func(string) error) {
 	// internal:是否为内部
 	// noWait:是否非阻塞, true为是,不等待RMQ返回信息;
 	// args:参数,传nil即可;
-	err := r.channel.ExchangeDeclare(r.queueConf.ExchangeName, r.queueConf.ExchangeType, true, false, false, false, nil)
+	err := r.Channel.ExchangeDeclare(r.ConsumerQueueConf.ExchangeName, string(r.ConsumerQueueConf.ExchangeType), true, false, false, false, nil)
 	if err != nil {
 		fmt.Printf("RabbitMQ注册交换机失败:%s \n", err)
 		return
 	}
 
 	// 用于检查队列是否存在,已经存在不需要重复声明
-	_, err = r.channel.QueueDeclare(r.queueConf.QueueName, true, false, false, false, nil)
+	_, err = r.Channel.QueueDeclare(r.ConsumerQueueConf.QueueName, true, false, false, false, nil)
 	if err != nil {
 		fmt.Printf("RabbitMQ注册队列失败:%s \n", err)
 		return
@@ -146,20 +151,20 @@ func (r *RabbitMQ) Consumer(doFunc func(string) error) {
 	// 绑定d队列
 	// name:队列名称;
 	// durable:是否持久化,队列存盘,true服务重启后信息不会丢失,影响性能;
-	// autoDelete:是否自动删除;
+	// autoDelete:是否自动删除; 最后一个Consumer取消订阅后，Queue是否自动删除。
 	// exclusive:是否设置排他
 	// noWait:是否非阻塞, true为是,不等待RMQ返回信息;
 	// args:参数,传nil即可;
-	err = r.channel.QueueBind(r.queueConf.QueueName, r.queueConf.RoutingKey, r.queueConf.ExchangeName, false, nil)
+	err = r.Channel.QueueBind(r.ConsumerQueueConf.QueueName, r.ConsumerQueueConf.RoutingKey, r.ConsumerQueueConf.ExchangeName, false, nil)
 	if err != nil {
 		fmt.Printf("RabbitMQ绑定队列失败:%s \n", err)
 		return
 	}
 	// 获取消费通道,确保rabbitMQ一个一个发送消息
-	err = r.channel.Qos(1, 0, true)
+	err = r.Channel.Qos(1, 0, true)
 
 	// 消息消费
-	msgList, err := r.channel.Consume(r.queueConf.QueueName, "miss-c-n", false, false, false, false, nil)
+	msgList, err := r.Channel.Consume(r.ConsumerQueueConf.QueueName, r.ConsumerQueueConf.ConsumerTag, false, false, false, false, nil)
 	if err != nil {
 		fmt.Printf("RabbitMQ获取消费通道异常:%s \n", err)
 		return
@@ -172,7 +177,7 @@ func (r *RabbitMQ) Consumer(doFunc func(string) error) {
 			err := doFunc(string(msg.Body))
 			fmt.Printf("RabbitMQ--Consume--doFunc监听消息执行结果 err:%s \n", err)
 			if err != nil {
-				err = msg.Ack(true)
+				err = msg.Nack(true, r.ConsumerQueueConf.Requeue)
 				if err != nil {
 					fmt.Printf("确认消息未完成异常:%s \n", err)
 				}
@@ -190,25 +195,25 @@ func (r *RabbitMQ) Consumer(doFunc func(string) error) {
 
 func (r *RabbitMQ) GetRabbitUrl() {
 	//rabbitUrl := fmt.Sprintf("amqp://%s:%s@%s:%d/", "guest", "guest", "******", 5673)
-	userName := r.connectConf.UserName
-	password := r.connectConf.Password
+	userName := r.ConnectConf.UserName
+	password := r.ConnectConf.Password
 	if len(userName) == 0 || len(password) == 0 {
-		userName = GetUserName(r.connectConf.AccessKey, r.connectConf.InstanceId)
-		password = GetPassword(r.connectConf.SecretKey)
+		userName = GetUserName(r.ConnectConf.AccessKey, r.ConnectConf.InstanceId)
+		password = GetPassword(r.ConnectConf.SecretKey)
 		if len(userName) == 0 || len(password) == 0 {
 			fmt.Printf("RabbitMQ--GetRabbitUrl--使用AccessKey和SecretKey动态获取用户名密码失败 \n")
 			return
 		}
 	}
-	fmt.Printf("RabbitMQ--GetRabbitUrl--使用AccessKey和SecretKey动态获取用户名 %s 密码 %s \n", userName, password)
-	r.rabbitUrl = fmt.Sprintf("amqp://%s:%s@%s", userName, password, r.connectConf.Endpoint)
-	if r.connectConf.Port != 0 {
-		r.rabbitUrl = fmt.Sprintf(`%s:%d`, r.rabbitUrl, r.connectConf.Port)
+
+	r.RabbitUrl = fmt.Sprintf("amqp://%s:%s@%s", userName, password, r.ConnectConf.Endpoint)
+	if r.ConnectConf.Port != 0 {
+		r.RabbitUrl = fmt.Sprintf(`%s:%d`, r.RabbitUrl, r.ConnectConf.Port)
 	}
-	if r.connectConf.Vhost != "" {
-		r.connectConf.Vhost = "default"
+	if len(r.ConnectConf.Vhost) == 0 {
+		r.ConnectConf.Vhost = "default"
 	}
 
-	r.rabbitUrl = fmt.Sprintf(`%s/%s`, r.rabbitUrl, r.connectConf.Vhost)
-	r.rabbitUrl += "?heartbeat=5"
+	r.RabbitUrl = fmt.Sprintf(`%s/%s`, r.RabbitUrl, r.ConnectConf.Vhost)
+	r.RabbitUrl += "?heartbeat=5"
 }
--- test.go
+++ test.go
@@ -13,38 +13,46 @@ var connect = rabbitmqGo.ConnectConf{
 	InstanceId: "",
 	AccessKey:  "",
 	SecretKey:  "",
-	Vhost:      "",
+	Vhost:      "devices",
 	Port:       0,
 }
 
-var exConf = rabbitmqGo.QueueExchange{
+var exPrdConf = rabbitmqGo.ProducerRoutingConf{
+	ExchangeName: "test-debug",
+	RoutingKey:   "debug-topic",
+}
+
+var exCsmConf = rabbitmqGo.ConsumerQueueConf{
+	ConsumerTag:  "test--miss--debug",
 	ExchangeName: "test-debug",
 	RoutingKey:   "debug-topic",
 	QueueName:    "debug-xin",
+	Requeue:      true,
 }
 
 func test() {
 
 	// 发送
-	rabbitmqGo.New(connect, exConf).Producer("miss-test---11111")
-	go runTime()
+	rabbitmqGo.New(connect, exPrdConf, exCsmConf).Producer("miss-test---222")
+	//go runTime()
 
 	// 消费
-	rabbitmqGo.New(connect, exConf).Consumer(doFunc)
+	rabbitmqGo.New(connect, exPrdConf, exCsmConf).Consumer(doFunc)
 
 	fmt.Println("9999999999--")
 	time.Sleep(10 * time.Minute)
 }
 
 func doFunc(msg string) error {
+	var err error
 	fmt.Println("Consumer消费信息--", msg)
-	return nil
+	return err
 }
 
 func runTime() {
-	C := time.Tick(1 * time.Second)
+	C := time.Tick(5 * time.Second)
 	for range C {
 		msg := fmt.Sprintf(`miss-test--%d`, time.Now().UnixMilli())
-		rabbitmqGo.New(connect, exConf).Producer(msg)
+		rabbitmqGo.New(connect, exPrdConf, exCsmConf).Producer(msg)
 	}
 }
